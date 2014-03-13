c4414cfe32462ea6607155da1ace08254543a568
idresign <idriss0said0alaoui@gmail.com>
2014-03-13 00:10:05 +0000

    first commit

---
+++ urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls.defaults import *
+from django.contrib import admin
+from django.conf import settings
+
+admin.autodiscover()
+
+urlpatterns = patterns(
+    '',
+    (r'^$', 'shortener.views.index'),    
+    (r'^admin/', include(admin.site.urls)),   
+    (r'^submit/$', 'shortener.views.submit'),
+    (r'^(?P<base62_id>\w+)$', 'shortener.views.follow'),
+    (r'^info/(?P<base62_id>\w+)$', 'shortener.views.info'),    
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+    {'document_root': settings.STATIC_DOC_ROOT}),
+)
